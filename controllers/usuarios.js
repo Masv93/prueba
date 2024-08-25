867e638bdd612a11cc85309298f8796d6530ec5d
Miguel Suárez <162487479+Masv93@users.noreply.github.com>
2024-08-24 20:19:09 -0400

    f

---
+++ controllers/usuarios.js
@@ -0,0 +1,83 @@
+const UsuarioModel = require('../models/usuarios');
+
+class UsuarioController {
+  
+    IniciarSesion(usuario) {
+       return new Promise((resolve, reject) => {
+        UsuarioModel.IniciarSesion(usuario)
+        .then((result) => {
+            resolve(result)
+        }).catch((err) => {
+            reject(err)
+        });
+       })
+    }
+   
+    Registrarse(usuario){
+        return new Promise((resolve, reject) => {
+            UsuarioModel.Registrarse(usuario)
+            .then((result) => {
+                resolve(result)
+            }).catch((err) => {
+                reject(err)
+            });
+        })
+    }
+
+    Decodificar(token){
+        return new Promise((resolve, reject) => {
+            UsuarioModel.Decodificar(token)
+            .then((result) => {
+                resolve(result)
+            }).catch((err) => {
+                reject(err)
+            });
+
+        })
+    }
+
+    CerrarSesion(cookie){
+        return new Promise((resolve, reject) => {
+            UsuarioModel.CerrarSesion(cookie)
+            .then(() => {
+                resolve()
+            }).catch((err) => {
+                reject(err)
+            });
+        })
+    }
+
+    Editar(id,datos){
+        return new Promise((resolve, reject) => {
+            UsuarioModel.Editar(id,datos)
+            .then(() => {
+                resolve()
+            }).catch((err) => {
+                reject(err)
+            });
+        })
+    }
+
+    Obtener(){
+        return new Promise((resolve, reject) => {
+            UsuarioModel.Obtener()
+            .then((result) => {
+                resolve(result)
+            }).catch((err) => {
+                reject(err)
+            });
+        })
+    }
+
+    BorrarUsuario(id){
+        return new Promise((resolve, reject) => {
+            UsuarioModel.BorrarUsuario(id)
+            .then(() => {
+                resolve()
+            }).catch((err) => {
+                reject(err)
+            });
+        })
+    }
+}
+module.exports = new UsuarioController();
---
+++ routes/usuarios.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const router = express.Router();
+const usuarioController = require("../controllers/usuarios");
+const studentsController = require("../controllers/studentsController");
+const usuarios = require("../models/usuarios");
+
+router.get("/", (req, res) =>
+    res.render("index")
+)
+router.post("/",(req,res)=>{
+    console.log(req.body)
+    usuarioController.IniciarSesion(req.body)
+    .then((usuario) => {
+        res.cookie("token",usuario)
+        res.redirect("/usuarios/Inicio")
+    }).catch((err) => {
+        res.render("error", { message: err.message, error: err });
+    });
+})
+router.get("/Inicio", (req, res) => {
+    usuarioController.Decodificar(req.cookies.token)
+        .then((result) => {
+            studentsController.Todos()
+                .then((students) => {
+                    res.render("Inicio", { token: result, students: students });
+                })
+                .catch((err) => {
+                    res.render("error", { message: err.message, error: err });
+                });
+        })
+        .catch((err) => {
+            res.render("error", { message: err.message, error: err });
+        });
+});
+router.get("/Registro",(req,res)=>{
+    res.render("Registro")
+})
+router.post("/Registro",(req,res)=>{
+    usuarioController.Registrarse(req.body)
+    .then(() => {
+            res.redirect("/usuarios")
+    }).catch((err) => {
+        console.error(err)
+        res.render("error", { message: err.message, error: err });
+    })
+    })
+router.get("/cerrar",(req,res)=>{
+    usuarioController.CerrarSesion(req.cookies.token)
+    .then(() => {
+        res.clearCookie("token")
+        res.redirect("/usuarios")
+    }).catch((err) => {
+        res.render("error", { message: err.message, error: err });
+    });
+})
+router.get("/editar",(req,res)=>{
+    usuarioController.Decodificar(req.cookies.token)
+    .then((result) => {
+        res.render("editarUsuario",{usuario:result})
+
+    }).catch((err) => {
+        res.render("error", { message: err.message, error: err });
+
+    });
+})
+router.put("/editar/:id",(req,res)=>{
+    usuarioController.Editar(req.params.id,req.body)
+    .then((result) => {
+        res.redirect("/usuarios")
+    }).catch((err) => {
+        res.render("error", { message: err.message, error: err });
+    });
+})
+
+router.delete("/borrar/:id",(req,res)=>{
+    usuarioController.BorrarUsuario(req.params.id)
+    .then(() => {
+        res.redirect("/usuarios/lista")
+    }).catch((err) => {
+        res.render("error", { message: err.message, error: err });
+    });
+})
+module.exports = router;
